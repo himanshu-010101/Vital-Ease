a261230894dd5a3b1753143953dee2814a24a53b
Himanshu <himanshusingh98912@gmail.com>
2026-05-10 01:42:46 +0530

    Database account changed

--- frontend/src/services/servProg.api.js
+++ frontend/src/services/servProg.api.js
@@ -18,6 +18,7 @@ export const createServProg = async (data) => {
 export const getAllServProg = async () => {
     try {
         const response = await api.get("/serv_prog");
+        console.log(response)
         return response.data;
     } catch (error) {
         console.error("Error fetching services or programmes:", error);
